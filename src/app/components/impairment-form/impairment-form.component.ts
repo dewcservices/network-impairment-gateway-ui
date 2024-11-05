c71c9cb61105046630701c39cfe206504b2476ac
Troy Boswell <troy.boswell@dewc.com>
2024-11-05 03:31:33 +0000

    refactor: impairment form unneccessary null check

--- src/app/components/impairment-form/impairment-form.component.ts
+++ src/app/components/impairment-form/impairment-form.component.ts
@@ -109,9 +109,9 @@ export class ImpairmentFormComponent
 
     this.impairmentService.impairmentSettings$.subscribe((impairment) => {
       if (impairment) {
-        this.model.bearer = impairment!.bearer_id
-        this.model.uplinkEnvironment = impairment!.bearer_id
-        this.model.downlinkEnvironment = impairment!.downlink_environment_id
+        this.model.bearer = impairment.bearer_id
+        this.model.uplinkEnvironment = impairment.bearer_id
+        this.model.downlinkEnvironment = impairment.downlink_environment_id
         this.form.patchValue({
           bearer: this.model.bearer,
           uplinkEnvironment: this.model.uplinkEnvironment,
